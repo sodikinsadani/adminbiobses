523dcaf6a05426d302a130466f72837d4eb49c82
sodikin <sodikinsadani@gmail.com>
2018-02-20 18:02:47 +0700

    get better

--- staticadm/admstatic/assets/js/lib/data-table/datatables-init.js
+++ staticadm/admstatic/assets/js/lib/data-table/datatables-init.js
@@ -8,8 +8,9 @@
 
 
 
-    $('#bootstrap-data-table').DataTable({
-        lengthMenu: [[10, 20, 50, -1], [10, 20, 50, "All"]],
+    table = $('#bootstrap-data-table').DataTable({
+		lengthMenu: [[10, 20, 50, -1], [10, 20, 50, "All"]],
+		select: true,
     });
 
 
